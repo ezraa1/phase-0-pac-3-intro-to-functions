f0ba3b0a5ab4c8719c183c544e0a4824420412f8
ezraa1 <ezrakipchirchir1034@gmail.com>
2022-06-01 10:21:55 +0300

    commit tree 3ce4de05ca42383fc9977263fe5cdbcb9c42e512

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Follow along with the examples here
+// function doNothing() {}
+// function sayHello() {
+//     console.log("Hello!");
+//   }
+//   sayHello();
+//   function sayHelloToGuadalupe() {
+//     console.log("Hello, Guadalupe!");
+//   }
+//   function sayHelloToLiz() {
+//     console.log("Hello, Liz!");
+//   }
+//   function sayHelloToSamip() {
+//     console.log("Hello, Samip!");
+//   }
+//   sayHelloToGuadalupe();
+//   sayHelloToLiz();
+//   sayHelloToSamip();
+//   function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+//   }
+//   say("Goodbye", "Julio");
+//   function add(x, y) {
+//     return x + y;
+//   }
+//   add(1, 2)       
+//   console.log(add(1, 2));
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+  }
+  console.log(say("Howdy", "partner"));
+  
